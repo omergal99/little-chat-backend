02f0c84437ee0aebf5a9d7a0a24c053eb4866637
omergal99 <omergal99gm@gmail.com>
2019-09-01 02:23:11 +0300

    first commit

---
+++ public/precache-manifest.bb571941008dadcefc27c530c3e742ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f786bf31e62ca49f28a36d039e67e57d",
+    "url": "/react-chat/index.html"
+  },
+  {
+    "revision": "caf73776575498a247d1",
+    "url": "/react-chat/static/css/main.c09388fa.chunk.css"
+  },
+  {
+    "revision": "9b80d36fab324ca36dc8",
+    "url": "/react-chat/static/js/2.9a7812a1.chunk.js"
+  },
+  {
+    "revision": "caf73776575498a247d1",
+    "url": "/react-chat/static/js/main.d7957159.chunk.js"
+  },
+  {
+    "revision": "49fde9a2889aba467256",
+    "url": "/react-chat/static/js/runtime~main.14e77f4a.js"
+  },
+  {
+    "revision": "56cb366910aa712773f8f2f7e7f42b9f",
+    "url": "/react-chat/static/media/Merienda-Regular.56cb3669.ttf"
+  },
+  {
+    "revision": "849e67957ea4dd5edb38eedb9d5743f1",
+    "url": "/react-chat/static/media/Quando-Regular.849e6795.ttf"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+var app = express();
+
+app.use(cors({
+  origin: ['http://localhost:3002'],
+  credentials: true // enable set cookie
+}));
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.send('Hello world')
+});
+
+io.on('connection', socket => {
+
+  console.log('an user connected');
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+
+  socket.on('msg sent', (txt, from) => {
+    console.log('we got msg:', txt, 'from', from)
+    io.emit('chat new msg', txt, from);
+  });
+
+  socket.on('user type', (user) => {
+    if (user) {
+      console.log('user type something:', user)
+      io.emit('other user type', user);
+    } else {
+      io.emit('other user type', '');
+    }
+  });
+
+});
+
+const port = process.env.PORT || 9090;
+http.listen(port, () => console.log(`server on *:${port}`));
