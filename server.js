355ae2e60e98ed525d9d7b8d0402e39c2df59fe6
omergal99 <omergal99gm@gmail.com>
2019-09-01 04:05:38 +0300

    omer push

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const session = require('express-session')
 
-var app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+
+const app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+const socketRoute = require('./routes/socket-route')
 
 app.use(cors({
   origin: ['http://localhost:3002'],
@@ -13,35 +17,15 @@ app.use(cors({
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(session({
+  secret: 'omer12565',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-  res.send('Hello world')
-});
-
-io.on('connection', socket => {
-
-  console.log('an user connected');
-
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
-
-  socket.on('msg sent', (txt, from) => {
-    console.log('we got msg:', txt, 'from', from)
-    io.emit('chat new msg', txt, from);
-  });
-
-  socket.on('user type', (user) => {
-    if (user) {
-      console.log('user type something:', user)
-      io.emit('other user type', user);
-    } else {
-      io.emit('other user type', '');
-    }
-  });
-
-});
+socketRoute(io)
 
 const port = process.env.PORT || 9090;
 http.listen(port, () => console.log(`server on *:${port}`));
